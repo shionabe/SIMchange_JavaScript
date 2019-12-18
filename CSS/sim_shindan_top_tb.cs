f6f3ddf3952ff8ff014d8cb2910a3d4592233342
shionabe <pdpq310.s.a@gmail.com>
2019-12-18 13:28:30 +0900

    cssボタンをレスポンシブに対応

--- CSS/sim_shindan_top_tb.cs
+++ CSS/sim_shindan_top_tb.cs
@@ -2,4 +2,22 @@
 
 @media (max-width:1000px) {
 
+  .twoColumn label{
+    float: left;
+    width:50%;
+  }
+  .fourColumn label{
+    float: left;
+    width:50%;
+  }
+
+  .q3block{
+    float: left;
+    width:50%;
+  }
+  .q3block label{
+    width:100%;
+  }
+
+
 }
