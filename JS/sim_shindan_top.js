ed4ffd9f52eb204f2d4802de36bc2d3408163888
shionabe <pdpq310.s.a@gmail.com>
2019-12-18 21:04:33 +0900

    レッスン後jQuery

--- JS/sim_shindan_top.js
+++ JS/sim_shindan_top.js
@@ -6,14 +6,25 @@ function q1Change(){
   if(radio[0].checked) {
    document.getElementById('q2-1').style.display = "";
    document.getElementById('q2-2').style.display = "none";
-
-}else if(radio[1].checked) {
- document.getElementById('q2-1').style.display = "none";
- document.getElementById('q2-2').style.display = "";
+    // var target = "#" + $(this).html();
+    var target = ".question_wrapper";
+    var th = $(target).position();
+    var sh = $(".question_wrapper").scrollTop();
+    var pos = th.top + sh + 1;
+    // var pos =  100;
+    // alert(pos);
+    // $(".question_wrapper").animate({
+    //   scrollTop: pos
+    // },"slow", "swing");
+    $(".question_wrapper").animate({ scrollTop: pos}, 500, 'swing');
+    // $('html, body').scrollTop(pos);
+  }else if(radio[1].checked) {
+   document.getElementById('q2-1').style.display = "none";
+   document.getElementById('q2-2').style.display = "";
 
 }
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 // 質問事項2
 function q2_1Change(){
@@ -21,7 +32,7 @@ function q2_1Change(){
   document.getElementById('q3').style.display = "block";
 
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 
 // 質問事項2
@@ -30,7 +41,7 @@ function q2_2Change(){
   document.getElementById('q3').style.display = "block";
 
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 
 // 質問事項3
@@ -39,7 +50,7 @@ function q3Change(){
   document.getElementById('q4').style.display = "block";
 
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 
 // 質問事項4
@@ -47,15 +58,16 @@ function q4Change(){
   radio = document.getElementsByName('q4')
   if(radio[0].checked) {
    document.getElementById('q5').style.display = "none";
-   document.getElementById('reslut').submit.disabled = false;
    // 結果ボタンの表示
-   // let formElements = document.forms.result;
-   // formElements.reslut_btn.disabled = false;
+   // document.getElementById('reslut').submit.disabled = false;
+   // 結果ボタンの表示
+   let formElements = document.forms.result;
+   formElements.result_btn.disabled = false;
 }else if(radio[1].checked) {
  document.getElementById('q5').style.display = "";
 }
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 
 
@@ -64,8 +76,11 @@ function q5Change(){
   radio = document.getElementsByName('q5')
   document.getElementById('reslut').style.display = "block";
   // 結果ボタンの表示
+  // 結果ボタンの表示
+  let formElements = document.forms.result;
+  formElements.result_btn.disabled = false;
 }
-window.onload = entryChange1;
+// window.onload = entryChange1;
 
 
 // 診断結果の表示/非表示
