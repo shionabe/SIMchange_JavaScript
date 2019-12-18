51c84fe8953a9366f468a5cdd455f954e8ce3513
shionabe <pdpq310.s.a@gmail.com>
2019-12-18 10:58:15 +0900

    結果の吐き出しができない

--- JS/sim_shindan_top.js
+++ JS/sim_shindan_top.js
@@ -6,9 +6,11 @@ function q1Change(){
   if(radio[0].checked) {
    document.getElementById('q2-1').style.display = "";
    document.getElementById('q2-2').style.display = "none";
+
 }else if(radio[1].checked) {
  document.getElementById('q2-1').style.display = "none";
  document.getElementById('q2-2').style.display = "";
+
 }
 }
 window.onload = entryChange1;
@@ -17,6 +19,7 @@ window.onload = entryChange1;
 function q2_1Change(){
   radio = document.getElementsByName('q2')
   document.getElementById('q3').style.display = "block";
+
 }
 window.onload = entryChange1;
 
@@ -25,6 +28,7 @@ window.onload = entryChange1;
 function q2_2Change(){
   radio = document.getElementsByName('q2')
   document.getElementById('q3').style.display = "block";
+
 }
 window.onload = entryChange1;
 
@@ -33,6 +37,7 @@ window.onload = entryChange1;
 function q3Change(){
   radio = document.getElementsByName('q3')
   document.getElementById('q4').style.display = "block";
+
 }
 window.onload = entryChange1;
 
@@ -42,10 +47,12 @@ function q4Change(){
   radio = document.getElementsByName('q4')
   if(radio[0].checked) {
    document.getElementById('q5').style.display = "none";
-   document.getElementById('reslut').style.display = "";
+   document.getElementById('reslut').submit.disabled = false;
+   // 結果ボタンの表示
+   // let formElements = document.forms.result;
+   // formElements.reslut_btn.disabled = false;
 }else if(radio[1].checked) {
  document.getElementById('q5').style.display = "";
- document.getElementById('reslut').style.display = "none";
 }
 }
 window.onload = entryChange1;
@@ -56,5 +63,12 @@ window.onload = entryChange1;
 function q5Change(){
   radio = document.getElementsByName('q5')
   document.getElementById('reslut').style.display = "block";
+  // 結果ボタンの表示
 }
 window.onload = entryChange1;
+
+
+// 診断結果の表示/非表示
+function resultChange(){
+
+}
